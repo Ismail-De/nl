8e42f62ef061910baf14c8429bb1a80f33e51652
Testin-it <84230658+Testin-it@users.noreply.github.com>
2022-11-26 23:34:53 +0100

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,23 +1,19 @@
 import streamlit as st
+from spacy_streamlit import *
 
 string1 = st.text_input("Your text here")
 
 def trsf(f):
   
-  import spacy
-  nlp = spacy.load('en_core_web_sm')
-  
   l =[]
-  doc = nlp(f)
-
-
+  doc = process_text('en_core_web_sm', f)
   for sent in doc.sents:
       print(sent)
       l+=[sent]
 
   m = string1.index(l[0])
   mm = string1.index(l[-1])
-  string1 = string1[m+len(l[0]) : mm] 
+  string1 = string1[m + len(l[0]) : mm] 
   return string1
 
 
